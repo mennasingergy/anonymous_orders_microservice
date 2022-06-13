dd943193eeb5f7c2ace23992ea3912f4a5027927
Menna Singergy <mennasingergy@gmail.com>
2022-06-13 02:02:16 +0200

    rgr

--- api/index.js
+++ api/index.js
@@ -3,14 +3,32 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const { mongoClient } = require('./mongo');
 const { uuid } = require('uuidv4');
+const cors= require('cors');
+
 
 const app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", value="*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods","POST,PATCH,GET,DELETE");
+  next();
+});
+app.use(function (req, res, next) {
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(cors({ origin : '*'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
